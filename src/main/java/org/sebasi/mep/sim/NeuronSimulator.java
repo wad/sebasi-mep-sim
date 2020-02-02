3a697bf0a319a839fcfbb853c9df25bc4957eea5
wad4ever <eric@wadhome.org>
2020-02-02 12:15:15 -0700

    Initial project

---
+++ src/main/java/org/sebasi/mep/sim/NeuronSimulator.java
@@ -0,0 +1,8 @@
+package org.sebasi.mep.sim;
+
+public class NeuronSimulator {
+    public static void main(String[] args) {
+//		System.out.println("NeuronSimulator " + NeuronSimulator.class.getPackage().getImplementationVersion());
+        System.out.println("NeuronSimulator");
+    }
+}
